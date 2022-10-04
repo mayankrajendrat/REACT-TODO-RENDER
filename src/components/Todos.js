75b74be5fbcce1db10ce28e6552068ebed657279
Mayank Tiwari <mayank_tiwari@epam.com>
2022-10-04 18:43:20 +0530

    React CodeBase for TODO APP

---
+++ src/components/Todos.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
+import {
+  addTask,
+  getTasks,
+  updateTask,
+  deleteTask,
+} from '../services/httpService'
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      let apiResponse;
+      addTask({"task":todo}).then(res=>{
+        apiResponse=res.task;
+        props.addTodo({
+          id: apiResponse["_id"],
+          item: apiResponse["task"],
+          completed: false,
+        });
+        setTodo("");
+      });
+    }
+  };
+  //console.log("props from store", props);
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => handleChange(e)}
+        className="todo-input"
+        value={todo}
+      />
+
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        className="add-btn"
+        onClick={() => add()}
+      >
+        <GoPlus />
+      </motion.button>
+      <br />
+    </div>
+  );
+};
+//we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
---
+++ src/services/httpService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const apiURL = "http://localhost:8000/api/todo";
+
+export function getTasks() {
+  return axios.get(apiURL + "s");
+}
+
+export async function addTask(task) {
+  try {
+    let response;
+    await axios.post(apiURL + "/create", task).then((res) => {
+      response = res.data;
+    });
+    return response;
+  } catch (error) {
+    console.error("ERROR:-" + error.message);
+  }
+}
+
+export async function updateTask(id, task) {
+    try {
+        let response;
+        await axios.put(apiURL + "/" + id + "/update", task).then((res) => {
+          response = res.data;
+        });
+        return response;
+      } catch (error) {
+        console.error("ERROR:-" + error.message);
+      }
+}
+
+export function deleteTask(id) {
+  return axios.delete(apiURL + "/" + id + "/delete");
+}
