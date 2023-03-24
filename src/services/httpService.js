15f2a6c2d53463e49f2293a5ad1a7282dc0bf402
shubhammandrawal <Shubham@7785>
2023-03-24 20:04:25 +0530

    commit

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiURL = "https://one0x-feb-23-node-todo-api.onrender.com/api/todo";
+const apiURL = "https://one0x-feb-23-node-todo-api.onrender.com/";
 
 export function getTasks() {
   return axios.get(apiURL + "s");
